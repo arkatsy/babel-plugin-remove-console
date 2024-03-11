a5c111e77616327386ff2150a850787973ce9574
Argyris <arkatsy@gmail.com>
2024-03-11 04:52:57 +0200

    refactor: remove-console plugin

--- remove-console/index.js
+++ remove-console/index.js
@@ -1,30 +1,50 @@
-module.exports = function ({ types: t }) {
+const t = require("@babel/types");
+
+module.exports = function () {
   return {
     name: "remove-console",
     visitor: {
-      VariableDeclarator(path) {
-        const init = path.node.init;
-
-        if (init.object.name === "console" && init.property.name === "log") {
-          path.node.init = t.functionExpression(null, [], t.blockStatement([]));
-        }
-      },
       CallExpression(path) {
-        const callee = path.node.callee;
+        const callee = path.get("callee");
         if (!t.isMemberExpression(callee)) return;
 
-        const parent = path.parent;
-        if (t.isLogicalExpression(parent) || t.isConditionalExpression(parent)) {
-          const void0 = t.unaryExpression("void", t.numericLiteral(0));
-          path.replaceWith(void0);
-          return;
-        }
-
-        if (callee.object.name === "console" && callee.property.name === "log") {
+        if (isExprConsoleLog(callee)) {
           path.remove();
-          return;
+        }
+      },
+      VariableDeclarator(path) {
+        const init = path.get("init");
+        if (!t.isMemberExpression(init)) return;
+
+        if (isExprConsoleLog(init)) {
+          const emptyFn = t.functionExpression(null, [], t.blockStatement([]));
+          init.replaceWith(emptyFn);
         }
       },
     },
   };
 };
+
+function isExprConsoleLog(node) {
+  return isSimpleConsoleLog(node) || isBindedConsoleLog(node);
+}
+
+function isSimpleConsoleLog(node) {
+  const object = node.get("object");
+  const property = node.get("property");
+  return (
+    t.isIdentifier(object.node, { name: "console" }) &&
+    t.isIdentifier(property.node, { name: "log" })
+  );
+}
+
+function isBindedConsoleLog(node) {
+  const object = node.get("object");
+  const property = node.get("property");
+
+  return (
+    t.isIdentifier(object.get("object").node, { name: "console" }) &&
+    t.isIdentifier(object.get("property").node, { name: "log" }) &&
+    ["bind", "call", "apply"].includes(property.node.name)
+  );
+}
---
+++ remove-console/plugin.test.js
@@ -0,0 +1,80 @@
+const { pluginTester } = require("babel-plugin-tester");
+const plugin = require("./index");
+const path = require("node:path");
+const { describe, it } = require("node:test");
+
+globalThis.describe = describe;
+globalThis.it = it;
+
+pluginTester({
+  plugin,
+  tests: [
+    {
+      title: "Remove top-level console.log",
+      code: `console.log(4);`,
+      output: ``,
+    },
+    {
+      title: "Remove simple block-scoped console.log",
+      code: `
+      function foo() { console.log(4); }
+      const bar = () => { console.log(4); }
+      if(true) { console.log(4); }`,
+      output: `
+      function foo() {}
+      const bar = () => {};
+      if (true) {
+      }`,
+    },
+    {
+      title: "Remove console.log from short-circuits (&& || ??)",
+      code: `
+      true && console.log(4);
+      console.log(4) || false;
+      console.log(4) ?? (false || true);
+      (false || (true && console.log(4))) ?? false;`,
+      output: `
+      true;
+      false;
+      false || true;
+      (false || true) ?? false;`,
+    },
+    {
+      title: "Handle re-assignent of console.log",
+      code: `
+      const log = console.log;
+      log(4);`,
+      output: `
+      const log = function () {};
+      log(4);`,
+    },
+    {
+      title: "Handle control-flow without block",
+      code: `
+      if (true) console.log(4)
+      for (;;) console.log(4)
+      for (const i in []) console.log(i)
+      for (const i of []) console.log(i)
+      while (true) console.log(4)
+      do console.log(4); while (false)`,
+      output: `
+      if (true) {
+      }
+      for (;;) {}
+      for (const i in []) {
+      }
+      for (const i of []) {
+      }
+      while (true) {}
+      do {} while (false);`,
+    },
+    {
+      title: "Remove console.log with context binding (.bind, .call, .apply)",
+      code: `
+      console.log.bind(console, 4);
+      console.log.call(console, 4);
+      console.log.apply(console, [4]);`,
+      output: ``,
+    },
+  ],
+});
